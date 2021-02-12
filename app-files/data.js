89e9548fbf36bad43f94c6c232d40176a13e4826
luigiparisi <78955241+luigiparisi@users.noreply.github.com>
2021-02-12 10:43:21 +0100

    Initial commit

---
+++ app-files/data.js
@@ -0,0 +1,56 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sanluca",
+      "name": "sanluca",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.061307008867085955,
+        "pitch": -0.1733010013213132,
+        "fov": 1.3977480336749475
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3350339333710792,
+          "pitch": -0.431955882404349,
+          "rotation": 0,
+          "target": "0-sanluca"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5824610712155653,
+          "pitch": -0.055142096444107835,
+          "title": "Prova",
+          "text": "bla bla bla"
+        }
+      ]
+    }
+  ],
+  "name": "sanluca",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
